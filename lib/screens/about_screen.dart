3d459b73a8d09c68bfc8bb594d9f24b7d1200052
GibinMGeorge <75688390+GibinMGeorge@users.noreply.github.com>
2022-07-12 22:14:55 +0530

    Passhub
    Passhub - Password Manager App

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -68,7 +68,10 @@ class _AboutScreenState extends State<AboutScreen> {
                 height: 60.0,
                 width: MediaQuery.of(context).size.width - 30.0,
                 decoration: BoxDecoration(
-                    color: Color(0xff2aa7f3),
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Color(0xff2aa7f3), Color(0xff2463f6)]),
                     borderRadius: BorderRadius.circular(8.0)),
                 child: Padding(
                     padding: const EdgeInsets.fromLTRB(10.0, 17.0, 10.0, 0.0),
@@ -82,8 +85,18 @@ class _AboutScreenState extends State<AboutScreen> {
               ),
               SizedBox(
                 height: 20.0,
-              )
-
+              ),
+              Container(
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 17.0, 10.0, 0.0),
+                  child: Center(child: Text(
+                    'Created by Gibin M George (AJC19IT023) , Roshan Reji(AJC19IT043) , Arjun Biju(AJC19IT016) , Christy Martin(AJC19IT020) as part of S6 Mini-Project .',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10.0,fontFamily: 'Ubuntu',
+                        fontWeight: FontWeight.w600),
+                  )),),
+              ),
             ],
           ),
         ));
